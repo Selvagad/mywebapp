ca9a6e757f70b29b5dd3901079102832b0dfabbb
Adam <selvaggio.adam@gmail.com>
2021-01-11 16:56:18 +0100

    add original files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using webapp.Models;
+
+namespace webapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly webappContext db;
+
+        public HomeController(ILogger<HomeController> logger, webappContext _db)
+        {
+            _logger = logger;
+            db = _db;
+        }
+
+        public IActionResult Index()
+        {
+            db.Database.EnsureCreated();
+            db.Persons.Add(new Person() { Name = "toto" });
+            db.SaveChanges();
+            ViewBag.nbPersonnes = db.Persons.Count();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/webappContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapp.Models
+{
+    public class webappContext : DbContext
+    {
+
+        public webappContext(DbContextOptions<webappContext> options)
+            :base(options)
+        {
+
+        }
+      
+
+        public virtual DbSet<Person> Persons { get; set; }
+    }
+
+}
